763d60030102d29005a1f8da87ba6b786f5cdba9
jaishankarh <jaishankarh@gmail.com>
2015-03-27 19:08:32 +0530

    Added some amount of documentation, 80% done

--- Labyrinth.h
+++ Labyrinth.h
@@ -18,32 +18,88 @@
 
 using namespace std;
 
+
+/**
+ *  A class that encapsulates a labyrinth...
+ */
 class Labyrinth {
     public:
+		/**
+		 * A constructor with 1 parameter, an input stream. This stream provides the input for initialising the labyrinth
+		 * @param input of type std::iostream used for initialising the matrix of the labyrinth.
+		 * This constructor takes in one parameter an input stream and initialises the Labyrinth, internally it also initialises all the components, like the connected_components, extremities, nodes_map.
+		 */
         Labyrinth(std::iostream &input);
         ~Labyrinth();
         std::vector< std::vector<char>* >* getMatrix();
         int getNoRows();
         int getNoCols();
+
+      /**
+		* A simple getter method returning the list of extremities. Extremities are nodes that have only one adjacent node. Basically like a dead end in a labyrinth
+		*/
         std::set<Node*>* getExtremities();
+
+       /**
+		* A simple getter method returning the list nodes that represent the longest path.
+		* Note that this function does not find the longest path, it simply returns it. The programmer needs to call, findLongestPath before getting this list.
+		*/
         std::list<Node*>* getLongest_Path();
 
+       /**
+		* A method that finds the longest path in the labyrinth. It traverses through connected nodes only.
+		*/
         void findLongest();
-        void traverseToNext(Node* n, Node* root,std::list<Node*> *path);
-        void findPath(Node* root,std::list<Node*> *path);
+
         void drawPath();
     private:
         //reads a file from the disk which contains the schema of the matrix... the #s and the dots..and load the other properties.
         void init_matrix(std::iostream &input);
+
+        /**
+		* A method that helps the getLongestPath() to find the longest path in the labyrinth. It traverses through connected nodes only.
+		*/
+		void findPath(Node* root,std::list<Node*> *path);
         std::vector< std::vector<char>* > *matrix;
+
+        /**
+		  * a private variable.
+		  * Number of rows in the matrix representing the labyrinth
+		  */
         int no_rows;
+        /**
+		  * a private variable.
+		  * Number of columns in the matrix representing the labyrinth
+		  */
         int no_cols;
+
+        /**
+		  * a private variable.
+		  * Length of the longest path. NUmber of nodes in that list
+		  */
         unsigned long max;
-        //std::list< Node& > all_nodes;
-        //std::list<Node*> *all_nodes;
+
+        /**
+		  * a private variable.
+		  * A map that stores nodes against their id as key. This is used for fast and easy fetching of a node, to fetch its details
+		  */
         std::map<int, Node*> *nodes_map;
+        /**
+		  * a private variable.
+		  * A collection of connected_components. connected_components is a set of nodes. Note that two nodes are connected, even if they are indirectly connected through other nodes. All such nodes that are part of one network belong to this collection. The labyrinth can have many such collections.
+		  */
         std::list< std::set<Node*>* > *connected_components;
+
+        /**
+		  * a private variable.
+		  * A collection of extremities. An extremity is defined as a node that has only one adjacent node. Basically like a dead end in a labyrinth.
+		  */
         std::set<Node*> *extremities;
+
+        /**
+		  * a private variable.
+		  * A collection of nodes that represent the longest path in the labyrinth.
+		  */
         std::list<Node*> *longest_path;
 };
 
--- Node.h
+++ Node.h
@@ -18,6 +18,9 @@
 
 using namespace std;
 
+/**
+ *  A class that encapsulates a Node that is connected in a graph. A collection of these form the Labyrinth
+ */
 class Node{
     private:
         //static int id_count = 0;
@@ -28,16 +31,46 @@ class Node{
         bool visited;
     public:
 
+        /**
+		 * A constructor with 2 parameters row and col.
+		 * @param row_no an integer depicting the row of this node in the labyrinth
+		 * @param col_no an integer depicting the column of this node in the labyrinth
+		 * This constructor uses these two parameters and produces an id which is unique in the labyrinth. Every node can be retrieved using this id.
+		 */
         Node(int row_no, int col_no);
 
         ~Node();
+
+       /**
+	    *A simple getter method returning the id
+	    */
         int getId();
-        int getNoRows();
-        int getNoCols();
+       /**
+		*A simple getter method returning the Row No
+		*/
+        int getRowNo();
+       /**
+		*A simple getter method returning the Col no
+		*/
+        int getColNo();
+       /**
+		*A simple getter method returning the list of adjacent nodes
+		*/
         std::list<Node*>* getAdj_Nodes();
+       /**
+		*A simple getter method to find out if the node is visited while traversal of finding the longest path.
+		* This is useful only to the findPath function..which makes the traversal
+		*/
         bool getVisited();
+       /**
+		*A simple getter method returning the id
+		* This sets the state of the node to visited
+		* @param v this parameter is a boolean value. If true, the node is visited, if false it is not visited.
+		*/
         void setVisited(bool v);
-        //function takes in a parameter which is the node that needs to be added as an adjacent node to the current node object. simply appends to the list
+        /**
+         *function takes in a parameter which is the node that needs to be added as an adjacent node to the current node object. simply appends to the list
+         */
         void add_adj(Node *n);
 };
 
